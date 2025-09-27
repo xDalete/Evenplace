444efd73a1ca98a3b67d9b6a04c1ccf4245c84e0
Weder Santos <wederpessoa202@gmail.com>
2025-09-27 20:08:00 -0300

    [ Front-end ] Páginas adicionais.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import CustomLink from "@/components/CustomLink";
-
+import CardEvento from "@/components/CardEvento";
 export default function Home() {
   return (
     <div
@@ -11,9 +11,11 @@ export default function Home() {
         padding: "20px",
       }}
     >
-      <h1 className="text-3xl font-bold underline">Hello, Evenplace!</h1>
+      <h1>Hello, Evenplace!</h1>
       <CustomLink href="/login">Login</CustomLink>
       <CustomLink href="/colors">Colors</CustomLink>
+      <CardEvento title="Trio Parada Dura" date="20 de Setembro, 2025" location="Praça do Bairro São Pedro" placesAvailable={2500} ticketPrice={40.00} ticketsAvailable={1800} hour={"18:00h às 00:00"}/> 
+
     </div>
   );
 }
